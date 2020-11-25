ac43acfd82307d7d5b948e80c72c74a31ab07db2
Tatiana <tatianabb21@gmail.com>
2020-11-24 20:39:33 -0500

    Portafolio

---
+++ js/up.js
@@ -0,0 +1,22 @@
+window.onscroll = function(){
+    if(document.documentElement.scrollTop>100){
+        document.querySelector(".goto").classList.add("show");
+    }else{
+        document.querySelector(".goto").classList.remove("show");
+    }
+}
+
+const navSlide = ()=>{    
+    const burger = document.querySelector(".menu__responsive");
+    const nav = document.querySelector(".menu");
+    var activeNav = document.querySelectorAll(".items li");
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('menu-active'); 
+        for(var i = 0; i<activeNav.length; i++){
+            activeNav[i].addEventListener('click',()=>{
+                nav.classList.remove('menu-active');
+            });
+        }
+    }); 
+}
+navSlide();
